7f6dfbd91da3b30f7bbd0704dfe3575cb646ec95
Akash <akash@devbook.(none)>
2010-03-16 09:16:16 +0530

    moonshine setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Billmebob::Application.routes.draw do |map|
+  devise_for :users
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -55,5 +57,6 @@ Billmebob::Application.routes.draw do |map|
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id(.:format)))'
-  root :to=>"Invoices#index"
+  root :to=>"invoices#index"
+
 end
